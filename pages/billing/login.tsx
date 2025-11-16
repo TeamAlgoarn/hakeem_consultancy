b2f0d9dc90dde1704ddd31c5ea1e9cc31bfd0223
KeerthiPNaik <164196185+KeerthiPNaik@users.noreply.github.com>
2025-11-16 16:30:07 +0530

    Updated the features for production.

--- pages/billing/login.tsx
+++ pages/billing/login.tsx
@@ -63,7 +63,7 @@ export default function BillingLogin() {
       setIsResetting(true);
 
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `${window.location.origin}/billing/reset-password`,
+        redirectTo: "https://www.studyvisum.com/billing/reset-password"
       });
 
       if (error) {
