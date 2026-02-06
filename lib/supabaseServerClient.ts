c682189e06609b0d22ec05eb05d7528cd81ba8f0
ashwiniritgithub <ashwinipatil38107@gmail.com>
2026-02-06 19:23:30 +0530

    fix supabase url env issue

--- lib/supabaseServerClient.ts
+++ lib/supabaseServerClient.ts
@@ -1,7 +1,26 @@
-//supabaseServerClient.ts 
-import { createClient } from '@supabase/supabase-js';
+// //supabaseServerClient.ts 
+// import { createClient } from '@supabase/supabase-js';
  
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+// const supabaseUrl = process.env.SUPABASE_URL!;
+// const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
  
-export const supabaseServer = createClient(supabaseUrl, supabaseServiceRoleKey);
+// export const supabaseServer = createClient(supabaseUrl, supabaseServiceRoleKey); 
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("supabaseUrl is required.");
+}
+
+if (!supabaseServiceRoleKey) {
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY is required.");
+}
+
+export const supabaseServer = createClient(
+  supabaseUrl,
+  supabaseServiceRoleKey
+);
